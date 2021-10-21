5666bc63fb8a208b771ed335a667a29306885756
nidhirvnair <73595631+nidhirvnair@users.noreply.github.com>
2021-10-21 17:25:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+song="";
+
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function preload(){
+   song=loadSound("music.mp3");
+}
+
+function play(){
+    song.play();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
